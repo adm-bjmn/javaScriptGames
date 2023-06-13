12ef7a6e0695ef612512472c86e5c539cafa116d
adm.bjmn <adm.bjmn@gmail.com>
2023-06-14 00:31:16 +0100

    RockPaperScissors finished - uncommented

--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -2,12 +2,49 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 
-// const possibleChoices = document.querySelectorAll('')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
 
-possibleChoices.forEach(choice => choice.addEventListener('click', (e) => {
+const possibleChoices = document.getElementsByClassName('button')
+const possibleChoicesArray = Array.from(possibleChoices);
+
+let userChoice
+let computerChoice
+let gameResult
+
+possibleChoicesArray.forEach(choice => choice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice();
+    generateResult();
 }))
-console.log(userChoice)
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+    console.log(randomNumber)
+    if (randomNumber === 1) {
+        computerChoice = 'Rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'Paper'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'Scissors'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function generateResult() {
+    if (computerChoice === userChoice) {
+        gameResult = 'Draw';
+    } else if (
+        (userChoice === 'Rock' && computerChoice === 'Scissors') ||
+        (userChoice === 'Paper' && computerChoice === 'Rock') ||
+        (userChoice === 'Scissors' && computerChoice === 'Paper')
+    ) {
+        gameResult = 'Win';
+    } else {
+        gameResult = 'Lose';
+    }
+    resultDisplay.innerHTML = gameResult
+
+}
+
